bed426fb4bb5702e015173dbdd7dc763d7e4cebb
Adriano <adriano.v.net@gmail.com>
2020-02-18 16:24:18 +0200

    Add project files.

---
+++ ExpensiveParser/Core/HtmlLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ExpensiveParser.Core
+{
+    public class HtmlLoader
+    {
+        readonly string url;
+        public HtmlLoader(IParserSettings settings)
+        {
+            url = $"{settings.BaseUrl}/{settings.Category}/{settings.Section}/{settings.FilterParam}";
+        }
+        public async Task<string> GetSource()
+        {
+            string source = null;
+            HttpClient client = new HttpClient();
+            HttpResponseMessage responce = await client.GetAsync(url);
+
+            if(responce != null && responce.StatusCode == HttpStatusCode.OK)
+            {
+                source = await responce.Content.ReadAsStringAsync();
+            }
+
+            return source;
+        }
+    }
+}
---
+++ ExpensiveParser/Core/ParserWorker.cs
@@ -0,0 +1,41 @@
+﻿using AngleSharp.Html.Parser;
+using AngleSharp.Html.Dom;
+using System;
+
+
+namespace ExpensiveParser.Core
+{
+    public class ParserWorker
+    {
+        private IParserSettings Settings;
+        private bool isActive;
+        private HtmlLoader loader;
+        public event Action<object> OnCompleted;
+        public ParserWorker()
+        {
+
+        }
+
+        public void Start()
+        {
+            isActive = true;
+            Worker();
+        }
+
+        private async void Worker()
+        {
+            for (int itemNumber = 0; itemNumber < Settings.LoadCount; itemNumber++)
+            {
+                if (!isActive)
+                {
+                    OnCompleted?.Invoke(this);
+                    return;
+                }
+                string source = await loader.GetSource();
+                HtmlParser Parser = new HtmlParser();
+                IHtmlDocument document = await Parser.ParseDocumentAsync(source);
+            }
+
+        }
+    }
+}
